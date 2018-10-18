fb21b9a3ce894e24b86eec1e13a790aed3078748
Patrick Luhmann <patrick.luhmann@gmail.com>
2018-10-17 20:33:59 -0700

    Add Models, UnitTest_Models; EFCore for them and ViewModels
    I had problems getting EFCore working on master, so I have created a branch
    from the past to try again to see if I can get it working smoothly.
    
    This is the first phase. The two new projects, Models and UnitTest_Models,
    are created, but they don't have any real code yet.
    
    EFCore, which had already been installed for ConsoleApp, has now been
    installed for Models, UnitTest_Models, and ViewModels. No database work
    has been done yet.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ UnitTest_Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Models")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Models")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d193423b-11b1-4515-8a5c-1f5eb2d4f376")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest_Models/UnitTest_Nutrition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace UnitTest_Models
+{
+	[TestClass]
+	public class UnitTest_Nutrition
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
